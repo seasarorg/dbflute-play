17dfbd1af190b147375bcca54a7f998c1266794f
manhole <manholex@gmail.com>
2014-03-07 14:43:19 +0900

    DebugControllerをsingletonでS2Containerへ登録して、リクエストを処理できるようにした。

--- dbflute-play-example/app/controllers/DebugController.java
+++ dbflute-play-example/app/controllers/DebugController.java
@@ -1,10 +1,19 @@
 package controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
 public class DebugController extends Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(DebugController.class);
+
+    public DebugController() {
+        logger.debug("<init>");
+    }
 
     public Result index() {
         return ok(index.render("DEBUG Your new application is ready."));
--- dbflute-play-example/app/framework/ApplicationGlobal.java
+++ dbflute-play-example/app/framework/ApplicationGlobal.java
@@ -2,6 +2,9 @@ package framework;
 
 import java.lang.reflect.Method;
 
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.container.servlet.S2ContainerDestroyer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +18,7 @@ import play.mvc.Http.Request;
 public class ApplicationGlobal extends GlobalSettings {
 
     private final Logger logger = LoggerFactory.getLogger(ApplicationGlobal.class);
+    private S2Container container;
 
     public ApplicationGlobal() {
         logger.debug("<init>");
@@ -24,19 +28,32 @@ public class ApplicationGlobal extends GlobalSettings {
     public void onStart(final Application app) {
         super.onStart(app);
         logger.debug("onStart: {}", app);
+
+        /*
+         * see: S2ContainerListener
+         * 
+         * SingletonS2ContainerInitializerはServletに依存しているので使えない。。。
+         */
+        //        SingletonS2ContainerInitializer initializer = new SingletonS2ContainerInitializer();
+        //        initializer.setConfigPath("app.dicon");
+        //        initializer.setApplication(null);
+        //        initializer.initialize();
+        SingletonS2ContainerFactory.init();
+        container = SingletonS2ContainerFactory.getContainer();
     }
 
     @Override
     public void onStop(final Application app) {
         logger.debug("onStop: {}", app);
+        S2ContainerDestroyer.destroy();
         super.onStop(app);
     }
 
     @Override
     public <A> A getControllerInstance(final Class<A> controllerClass) throws Exception {
         logger.debug("getControllerInstance: {}", controllerClass);
-        return controllerClass.newInstance();
-        //return super.getControllerInstance(controllerClass);
+        final A component = (A) container.getComponent(controllerClass);
+        return component;
     }
 
     @Override
