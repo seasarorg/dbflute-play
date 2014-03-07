b1108954a3fb802762b947224e1e5ebb43672617
manhole <manholex@gmail.com>
2014-03-07 13:17:00 +0900

    Action実行前後でログを出すようにした

--- dbflute-play-example/app/framework/ApplicationGlobal.java
+++ dbflute-play-example/app/framework/ApplicationGlobal.java
@@ -7,12 +7,14 @@ import org.slf4j.LoggerFactory;
 
 import play.Application;
 import play.GlobalSettings;
+import play.libs.F.Promise;
 import play.mvc.Action;
+import play.mvc.Http.Context;
 import play.mvc.Http.Request;
 
 public class ApplicationGlobal extends GlobalSettings {
 
-    private static final Logger logger = LoggerFactory.getLogger(ApplicationGlobal.class);
+    private final Logger logger = LoggerFactory.getLogger(ApplicationGlobal.class);
 
     public ApplicationGlobal() {
         logger.debug("<init>");
@@ -40,7 +42,28 @@ public class ApplicationGlobal extends GlobalSettings {
     @Override
     public Action onRequest(final Request request, final Method actionMethod) {
         logger.debug("onRequest: {}", request);
-        return super.onRequest(request, actionMethod);
+        final Action action = super.onRequest(request, actionMethod);
+        return new AppAction(action);
+    }
+
+    static class AppAction extends Action {
+
+        private final Logger logger = LoggerFactory.getLogger(AppAction.class);
+
+        public AppAction(final Action delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public Promise call(final Context ctx) throws Throwable {
+            logger.debug("call before");
+            try {
+                final Promise result = delegate.call(ctx);
+                return result;
+            } finally {
+                logger.debug("call after");
+            }
+        }
     }
 
 }
