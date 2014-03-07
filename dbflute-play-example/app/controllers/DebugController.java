1b42f159eab59e153b450b8250ff8a85770f1967
manhole <manholex@gmail.com>
2014-03-07 16:09:29 +0900

    debug画面へDatabaseMetaDataを表示してみた

--- dbflute-play-example/app/controllers/DebugController.java
+++ dbflute-play-example/app/controllers/DebugController.java
@@ -1,5 +1,14 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+import javax.sql.DataSource;
+
+import org.seasar.extension.jdbc.util.ConnectionUtil;
+import org.seasar.extension.jdbc.util.DataSourceUtil;
+import org.seasar.framework.container.annotation.tiger.Binding;
+import org.seasar.framework.container.annotation.tiger.BindingType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +19,22 @@ public class DebugController extends Controller {
 
     private static final Logger logger = LoggerFactory.getLogger(DebugController.class);
 
+    @Binding(bindingType = BindingType.MUST)
+    private DataSource dataSource;
+
     public DebugController() {
         logger.debug("<init>");
     }
 
     public Result index() {
-        return ok(views.html.debug.render("DEBUG Your new application is ready."));
+        final Connection conn = DataSourceUtil.getConnection(dataSource);
+        try {
+            final DatabaseMetaData metaData = ConnectionUtil.getMetaData(conn);
+            final Status ret = ok(views.html.debug.render("DEBUG Your new application is ready.", metaData));
+            return ret;
+        } finally {
+            ConnectionUtil.close(conn);
+        }
     }
 
 }
