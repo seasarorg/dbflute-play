84ff3dffd4b92a5b505c51d3088f37260c723a81
manhole <manholex@gmail.com>
2014-03-12 16:21:21 +0900

    S2のcooldeployを有効にし、controllerを登録する。
    controllersのネーミングがS2標準のNamingConventionImplと合わないので、Playアプリ用NamingConventionを作成した。

---
+++ dbflute-play-example/app/framework/ControllerCreator.java
@@ -0,0 +1,31 @@
+package framework;
+
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.creator.ComponentCreatorImpl;
+import org.seasar.framework.container.deployer.InstanceDefFactory;
+import org.seasar.framework.convention.NamingConvention;
+
+import play.mvc.Controller;
+
+public class ControllerCreator extends ComponentCreatorImpl {
+
+    public ControllerCreator(final NamingConvention namingConvention) {
+        super(namingConvention);
+        setNameSuffix("Controller");
+        setInstanceDef(InstanceDefFactory.SINGLETON);
+    }
+
+    @Override
+    public ComponentDef createComponentDef(final Class componentClass) {
+        if (!Controller.class.isAssignableFrom(componentClass)) {
+            /*
+             * playが生成するControllerではないクラス
+             * (routesやReverseXxxxController)を除外する
+             */
+            return null;
+        }
+        final ComponentDef cd = super.createComponentDef(componentClass);
+        return cd;
+    }
+
+}
